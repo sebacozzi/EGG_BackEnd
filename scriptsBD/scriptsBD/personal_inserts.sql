9cc4f979bbf00a7ec310726e518b7c4bbdbc27b2
Sebastian Cozzi <sebacozzi26@gmail.com>
2023-06-14 08:42:01 -0300

    Ejercicio 1 Guia 14 - MySQL

--- scriptsBD/scriptsBD/personal_inserts.sql
+++ scriptsBD/scriptsBD/personal_inserts.sql
@@ -67,3 +67,94 @@ INSERT INTO `empleados` VALUES (338,'Abel Gómez','M','1939-12-24','2000-10-01',
 INSERT INTO `empleados` VALUES (689,'Mario Llano','M','1945-08-30','1990-05-16',2250000,2500000,'Vendedor',2300);
 INSERT INTO `empleados` VALUES (785,'Joaquín Rosas','M','1947-07-07','1990-05-16',2250000,2500000,'Vendedor',2200);
 INSERT INTO `empleados` VALUES (898,'Iván Duarte','M','1955-08-12','1998-05-16',1050000,200000,'Mecánico',4100);
+
+ -- 1. Obtener los datos completos de los empleados.
+select * from departamentos;
+
+-- 2. Obtener los datos completos de los departamentos.
+SELECT * FROM empleados;
+
+-- 3. Listar el nombre de los departamentos.
+select nombre_depto from departamentos;
+
+-- 4. Obtener el nombre y salario de todos los empleados.
+select nombre,sal_emp from empleados;
+
+-- 5. Listar todas las comisiones.
+select comision_emp from empleados;
+
+-- 6. Obtener los datos de los empleados cuyo cargo sea ‘Secretaria’.
+select * from empleados where UPPER(cargo_emp) = 'SECRETARIA';
+
+-- 7. Obtener los datos de los empleados vendedores, ordenados por nombre alfabéticamente.
+select * from empleados where UPPEr(cargo_emp) = 'VENDEDOR' order by nombre;
+
+-- 8. Obtener el nombre y cargo de todos los empleados, ordenados por salario de menor a mayor.
+select nombre, cargo_emp from empleados order by sal_emp;
+
+-- 9. Obtener el nombre de o de los jefes que tengan su departamento situado en la ciudad de “Ciudad Real”
+select nombre from empleados where upper(cargo_emp) like 'JEFE%' and id_depto in (select id_depto from departamentos where UPPER(ciudad) = 'CIUDAD REAL');
+
+-- 10. Elabore un listado donde para cada fila, figure el alias ‘Nombre’ y ‘Cargo’ para las respectivas tablas de empleados.
+select nombre as Nombre, cargo_emp as Cargo from empleados;
+
+-- 11. Listar los salarios y comisiones de los empleados del departamento 2000, ordenado por comisión de menor a mayor.
+select sal_emp,comision_emp from empleados where id_depto = 2000 order by comision_emp; 
+
+/* 12. Obtener el valor total a pagar a cada empleado del departamento 3000, que resulta 
+de: sumar el salario y la comisión, más una bonificación de 500. Mostrar el nombre del
+empleado y el total a pagar, en orden alfabético.
+*/
+select nombre,(sal_emp + comision_emp + 500) as Sueldo from empleados where id_depto = 3000 order by nombre;
+
+-- 13. Muestra los empleados cuyo nombre empiece con la letra J.
+select * from empleados where nombre like 'J%';
+
+-- 14. Listar el salario, la comisión, el salario total (salario + comisión) y nombre, de aquellos empleados que tienen comisión superior a 1000.
+select sal_emp, comision_emp,(sal_emp + comision_emp) as Sueldo, nombre from empleados where comision_emp>1000;
+
+-- 15. Obtener un listado similar al anterior, pero de aquellos empleados que NO tienen comisión.
+select sal_emp, comision_emp,(sal_emp + comision_emp) as Sueldo, nombre from empleados where comision_emp=0;
+
+-- 16. Obtener la lista de los empleados que ganan una comisión superior a su sueldo.
+select * from empleados where comision_emp>sal_emp;
+
+-- 17. Listar los empleados cuya comisión es menor o igual que el 30% de su sueldo.
+select * from empleados where comision_emp<=(sal_emp*0.3); 
+
+-- 18. Hallar los empleados cuyo nombre no contiene la cadena “MA”
+select * from empleados where nombre not like 'MA';
+select * from empleados where UPPER(nombre) not like 'MA';
+
+-- 19. Obtener los nombres de los departamentos que sean “Ventas”, “Investigación” o ‘Mantenimiento.
+select * from departamentos where lower(nombre_depto) in ('ventas', 'investigación', 'mantenimiento');
+select * from departamentos;
+
+-- 20. Ahora obtener el contrario, los nombres de los departamentos que no sean “Ventas” ni “Investigación” ni ‘Mantenimiento.
+select * from departamentos where lower(nombre_depto) not in ('ventas', 'investigación', 'mantenimiento');
+
+-- 21. Mostrar el salario más alto de la empresa.
+select max(sal_emp) as Salario_Maximo from empleados;
+
+-- 22. Mostrar el nombre del último empleado de la lista por orden alfabético.
+
+
+-- 23. Hallar el salario más alto, el más bajo y la diferencia entre ellos.
+
+
+-- 24. Hallar el salario promedio por departamento. 
+
+
+-- Consultas con Having
+-- 25. Hallar los departamentos que tienen más de tres empleados. Mostrar el número de empleados de esos departamentos.
+
+
+-- 26. Hallar los departamentos que no tienen empleados
+
+
+-- Consulta Multitabla (Uso de la sentencia JOIN/LEFT JOIN/RIGHT JOIN)
+-- 27. Mostrar la lista de empleados, con su respectivo departamento y el jefe de cada departamento.
+
+
+-- Consulta con Subconsulta
+-- 28. Mostrar la lista de los empleados cuyo salario es mayor o igual que el promedio de la empresa. Ordenarlo por departamento.
