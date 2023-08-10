0ca025e24153768cb3693d499c7db391b98cead5
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2023-08-10 11:17:50 -0300

    Carga Ejercicio 25 - Guia 18

--- Guia18/Ejercicios/ej25/script.js
+++ Guia18/Ejercicios/ej25/script.js
@@ -1,39 +1,24 @@
+
 window.onload = function () {
     const url = new URLSearchParams(window.location.search);
-    if (typeof (parametros) == "undefined") {
-        var parametros = url.get("params");
+        let parametros;
+    if ( url.get("params")==null) {
+        parametros =sessionStorage.getItem("activo");
+        } else{
+       sessionStorage.setItem("activo", url.get("params"));
+       parametros =sessionStorage.getItem("activo");
     }
     document.title = "Ejercicio " + parametros + " - Guia 18";
     document.getElementById("titulo").innerHTML = "Ejercicio " + parametros + " - Guia 18";
     document.getElementById("consigna").innerHTML = decodeURIComponent(atob(sessionStorage.getItem(parametros)));
-    document.getElementById("tituloform").innerHTML = `Apriete el botón para resaltar las palabras`;
+    document.getElementById("tituloform").innerHTML = `Obtener datos del formulario`;
+    
+    
 };
 
-
-function completarFormulario() {
-    const fr = document.getElementById("formulario");
-    const texto = document.getElementById("texto");
-
-    const resaltar = (txt) => {
-        let palabras = txt.split(" ");
-        console.log(txt);
-        console.log(palabras);
-        palabras = palabras.map((palabra)=>{
-            if (palabra.length>8){
-                palabra= `<span class=\"resaltado-amarillo\">${palabra}</span>`;
-            }
-            return palabra;
-        });
-        return palabras.join(" ");
-    };
-
-    if (!fr.resaltado) {
-        document.getElementById("tituloform").innerHTML = "Apriete el botón para ocultar el resaltado";
-        texto.innerHTML = resaltar(texto.textContent);
-    } else {
-        document.getElementById("tituloform").innerHTML = "Apriete el botón para resaltar las palabras";
-        texto.innerHTML = texto.textContent;
-    }
-
-    fr.resaltado = !fr.resaltado;
-};
+document.getElementById("form1").addEventListener("submit",(ev)=>{
+    
+    const fr= document.getElementById("form1");
+    alert(`Nombre: ${fr.nombre.value}
+Apellido: ${fr.apellido.value}`);
+},false);
