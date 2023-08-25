0196b92127ff5f5a55a29c994024636d8ae871e0
Sebastian Cozzi <sebacozzi26@gmail.com>
2023-08-24 23:55:10 -0300

    Carga organizador Ejercicios(Practica)

---
+++ Guia19/Ejercicios/ejercicios/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Guia19/Ejercicios/ejercicios/src/components/Ejercicio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ejercicio() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
