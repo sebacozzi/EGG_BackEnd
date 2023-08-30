92e634d7006a609430cb53f6fe2eaf5d4b298145
Sebastian Cozzi <sebacozzi26@gmail.com>
2023-08-29 23:42:22 -0300

    Actualización

--- Guia19/Ejercicios/tetrix/src/components/public/Pieza.jsx
+++ Guia19/Ejercicios/tetrix/src/components/public/Pieza.jsx
@@ -6,7 +6,7 @@ export default function Pieza({ficha,x,y,onMouseMove}) {
 
     return (
         <div className='ficha'>
-                <div key={1} className={`casilla ficha ficha-${ficha[0]}`} style={{top:(y)*30+1,left:((x)*30)+1,width:(30*ficha[1])-2, height: 28 }} onMouseMove={onMouseMove}></div>
+                <div key={1} className={`casilla ficha ficha-${ficha}`} style={{top:(y)*30+1,left:((x)*30)+1,width:(30*ficha)-2, height: 28 }} onMouseMove={onMouseMove}></div>
  
         </div>
     )
--- Guia19/Ejercicios/tetrix/src/components/public/Tablero.jsx
+++ Guia19/Ejercicios/tetrix/src/components/public/Tablero.jsx
@@ -19,68 +19,81 @@ export default function Tablero() {
     const [casillas, setCasillas] = useState(Array.from({ length: filas }, (v, f) => Array.from({ length: columnas }, (v, i) => {
         if (f === filas) { return 1 } return 0
     })));
-
     const [segundos, setSegundos] = useState(0);
+    const [nuevaFila, setNuevaFila] = useState([]);
+    
 
     const [fichas, setFichas] = useState(
-        [{ pieza: pieza1, x: 2, y: 19 },
-        { pieza: pieza3, x: 12, y: 19 },
-        { pieza: pieza4, x: 4, y: 18 },
-        { pieza: pieza4, x: 7, y: 19 },
-        { pieza: pieza3, x: 4, y: 19 },
+        [{ pieza: 1, x: 2, y: 19 },
+        { pieza: 3, x: 12, y: 19 },
+        { pieza: 4, x: 4, y: 18 },
+        { pieza: 4, x: 7, y: 19 },
+        { pieza: 3, x: 4, y: 19 },
         ]
     )
 
-    useEffect(() => {
+    useEffect(()=>{
+        if(nuevaFila.length!== 0){
+            const subeFichas= fichas.map((ficha)=>{ficha.y--
+            return ficha});
+            setFichas(fichas=>[...subeFichas,...nuevaFila] )
+        console.log('sube fila '+nuevaFila.length)}
+        console.log('Nueva fila')
+        console.log(fichas)
+    },[nuevaFila])
+     useEffect(() => {
         
         const intervalo = setInterval(() => {
             /// Sube las fichas en el tablero 1 posición
-            setFichas(fichas=>fichas.map((ficha) => {
-                ficha.y--;
-                return ficha;
-            }) )
+            
            
             //// Crea la nueva linea de Fichas
-            let nFichas = [];
-            let largoMax = 4;
-           /*  do { */
-                
+         
+            setSegundos(segundos+1);
             
-            for (let index = 0; index < columnas; index++) {
-               
-                if ( (columnas-index) < 4) { largoMax = index - columnas }
-                
-                if(largoMax>0){
-                    const llevaFicha=Math.round(Math.random());
-                if (llevaFicha === 1) {
-                    const tipoFicha = Math.round(Math.random() * largoMax) + 1;
-                    const nPieza = [tipoFicha, tipoFicha];
-                    const y = 19;
-                    const x = index;
-                    index += tipoFicha;
-                    nFichas = nFichas.concat([{ pieza:  nPieza , x: x , y:  y  }]);
-                }}
-            }
-            console.log('Longitud nFicha: '+nFichas.length)
-       /*  } while (nFichas.length===0);  */
-       if(segundos>10){ clearInterval(intervalo);}
-       console.log(nFichas)
-            setFichas(fichas => fichas.concat(nFichas)); 
-            setSegundos(segundos=>segundos+1);
-        },5000); 
+            
+            
+            
+        
+        },1000); 
+        
         return ()=>{ clearInterval(intervalo)}
-    },[])
+        
+    },[]) 
     function handleMouseMove(indice, ficha) {
-
-        setMOver('Tipo ficha: ' + ficha[0] + ' Indice: ' + indice);
+        setMOver('Tipo ficha: ' + ficha + ' Indice: ' + indice);
     }
-    console.log(fichas)
+    
+    const handleOnClick=(e)=>{
+        let nFichas = [];
+        let largoMax = 4;
+
+        for (let index = 0; index < columnas; index++) {
+               
+            if ( (columnas-index) < 5) { largoMax = index - columnas  }
+            
+            if(largoMax>0){
+                const llevaFicha=Math.round(Math.random());
+            if (llevaFicha === 1) {
+                const tipoFicha = Math.round(Math.random() * largoMax) + 1;
+                const nPieza = tipoFicha;
+                const y = 19;
+                const x = index;
+                index += tipoFicha;
+                nFichas = nFichas.concat([{ pieza:  nPieza , x: x , y:  y  }]);
+            }}
+        }
+        setNuevaFila(nFichas);
+        
+    }
+
+   
     return (
         <div>
             {/* Tablero de Puntos */}
             <div className='tanteador' style={{ position: 'absolute', left: '10px', height: '150px', width: '150px' }}><p>{mOver}</p><p>{segundos}</p></div>
             {/* Dibuja base tablero */}
-            <div className='tablero' >
+            <div className='tablero' onClick={handleOnClick}>
                 {casillas.map((fila, iFila) => (
                     <div key={iFila} className='fila'>
                         {fila.map((columna, iColumna) => (
